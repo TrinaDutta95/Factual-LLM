25072d0c549190c7c5c1f2fb27a83f5c6798e3e7
Trina <trinadutta2012@gmail.com>
2025-02-03 14:10:46 -0500

    removed files with dataset

--- load_xsum.py
+++
@@ -1,6 +0,0 @@
-from datasets import Dataset
-
-def load_xsum():
-    # Load the dataset from the local disk path
-    dataset = Dataset.load_from_disk("xsum_factual_combined_with_gold")
-    return dataset
